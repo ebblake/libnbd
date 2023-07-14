ebblake/libnbd

commit 6f4dcdab83aed82188ef712a3a2efa0f7fcbb79f

docs: Clarify how callbacks should handle errors
Recent patches have demonstrated end-user confusion on the order in
which callbacks are reached, when it is safe or dangerous to ignore
*error, and what a completion callback should do when auto-retirement
is in use.  Add wording to make it more obvious that:

- callbacks are reached in the following order: mid-command callback
  (0, 1, or many times, if supplied), completion callback (exactly
  once, if supplied), mid-command free (exactly once, if supplied),
  completion free (exactly once, if supplied)
- returning -1 from a mid-command callback does does not prevent
  future callbacks
- ignoring *error in a mid-command callback is safe
- completion callbacks are reached unconditionally, and must NOT ignore
  *error
- if the user chooses to use auto-retirement instead of manual calls to
  nbd_aio_command_completed, the completion callback should return 1 even
  on error cases to avoid complicating command cleanup
- the contents of buf after nbd_pread and friends is undefined on
  error (at present, if the user did not pre-initialize the buffer,
  there are some code paths in libnbd that leave it uninitialized)

Message-Id: <20220203202558.203013-4-eblake@redhat.com>
Reviewed-by: Laszlo Ersek <lersek@redhat.com>
Acked-by: Nir Soffer <nsoffer@redhat.com>

docs/libnbd.pod
@@ -829,8 +829,12 @@ This can be used to free associated C<user_data>.  For example:
                 NBD_NULL_COMPLETION,
                 0);
 
-will call L<free(3)> on C<my_data> after the last time that the
-S<C<chunk.callback = my_fn>> function is called.
+will call L<free(3)> once on C<my_data> after the point where it is
+known that the S<C<chunk.callback = my_fn>> function can no longer be
+called, regardless of how many times C<my_fn> was actually called.  If
+both a mid-command and completion callback are supplied, the functions
+will be reached in this order: mid-function callbacks, completion
+callback, mid-function free, and finally completion free.
 
 The free function is only accessible in the C API as it is not needed
 in garbage collected programming languages.
@@ -858,27 +862,60 @@ same nbd object, as it would cause deadlock.
 
 =head2 Completion callbacks
 
-All of the low-level commands have a completion callback variant that
-registers a callback function used right before the command is marked
-complete.
+All of the asychronous commands have an optional completion callback
+function that is used right before the command is marked complete,
+after any mid-command callbacks have finished, and before any free
+functions.
 
 When the completion callback returns C<1>, the command is
 automatically retired (there is no need to call
-L<nbd_aio_command_completed(3)>); for any other return value, the command
-still needs to be retired.
+L<nbd_aio_command_completed(3)>); for any other return value, the
+command still needs to be manually retired (otherwise, the command
+will tie up resources until L<nbd_close(3)> is eventually reached).
 
 =head2 Callbacks with C<int *error> parameter
 
 Some of the high-level commands (L<nbd_pread_structured(3)>,
-L<nbd_block_status(3)>) involve the use of a callback function invoked by
-the state machine at appropriate points in the server's reply before
-the overall command is complete.  These callback functions, along with
-all of the completion callbacks, include a parameter C<error>
-containing the value of any error detected so far; if the callback
-function fails, it should assign back into C<error> and return C<-1>
-to change the resulting error of the overall command.  Assignments
-into C<error> are ignored for any other return value; similarly,
-assigning C<0> into C<error> does not have an effect.
+L<nbd_block_status(3)>) involve the use of a callback function invoked
+by the state machine at appropriate points in the server's reply
+before the overall command is complete.  These callback functions,
+along with all of the completion callbacks, include a parameter
+C<error> which is a pointer containing the value of any error detected
+so far.  If a callback function fails and wants to change the
+resulting error of the overall command visible later in the API
+sequence, it should assign back into C<error> and return C<-1> in the
+C API.  Assignments into C<error> are ignored for any other return
+value; similarly, assigning C<0> into C<error> does not have an
+effect.  In other language bindings, reporting callback errors is
+generally done by raising an exception rather than by return value.
+
+Note that a mid-command callback might never be reached, such as if
+libnbd detects that the command was invalid to send (see
+L<nbd_set_strict_mode(3)>) or if the server reports a failure that
+concludes the command.  It is safe for a mid-command callback to
+ignore non-zero C<error>: all the other parameters to the mid-command
+callback will still be valid (corresponding to the current portion of
+the server's reply), and the overall command will still fail (at the
+completion callback or L<nbd_aio_command_completed(3)> for an
+asynchronous command, or as the result of the overall synchronous
+command).  Returing C<-1> from a mid-command callback does not prevent
+that callback from being reached again, if the server sends more
+mid-command replies that warrant another use of that callback.  A
+mid-command callback may be reached more times than expected if the
+server is non-compliant.
+
+On the other hand, if a completion callback is supplied (only possible
+with asynchronous commands), it will always be reached exactly once,
+and the completion callback must not ignore the value pointed to by
+C<error>.  In particular, the content of a buffer passed to
+L<nbd_aio_pread(3)> or L<nbd_aio_pread_structured(3)> is undefined
+if C<*error> is non-zero on entry to the completion callback.  It is
+recommended that if you choose to use automatic command retirement
+(where the completion callback returns C<1> to avoid needing to call
+L<nbd_aio_command_completed(3)> later), your completion function
+should return C<1> on all control paths, even when handling errors
+(note that with automatic retirement, assigning into C<error> is
+pointless as there is no later API to see that value).
 
 =head1 COMPILING YOUR PROGRAM
 

generator/API.ml
@@ -1,6 +1,6 @@
 (* hey emacs, this is OCaml code: -*- tuareg -*- *)
 (* nbd client library in userspace: the API
- * Copyright (C) 2013-2021 Red Hat Inc.
+ * Copyright (C) 2013-2022 Red Hat Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -1829,7 +1829,9 @@ may enforce a smaller limit, which can be learned with
 L<nbd_get_block_size(3)>.
 
 The C<flags> parameter must be C<0> for now (it exists for future NBD
-protocol extensions)."
+protocol extensions).
+
+Note that if this command fails, the contents of C<buf> are undefined."
 ^ strict_call_description;
     see_also = [Link "aio_pread"; Link "pread_structured";
                 Link "get_block_size"; Link "set_strict_mode"];
@@ -1914,7 +1916,9 @@ The C<flags> parameter may be C<0> for no flags, or may contain
 C<LIBNBD_CMD_FLAG_DF> meaning that the server should not reply with
 more than one fragment (if that is supported - some servers cannot do
 this, see L<nbd_can_df(3)>). Libnbd does not validate that the server
-actually obeys the flag."
+actually obeys the flag.
+
+Note that if this command fails, the contents of C<buf> are undefined."
 ^ strict_call_description;
     see_also = [Link "can_df"; Link "pread";
                 Link "aio_pread_structured"; Link "get_block_size";
@@ -2155,7 +2159,8 @@ C<NBD_REPLY_TYPE_BLOCK_STATUS> describes the meaning of this array;
 for contexts known to libnbd, B<E<lt>libnbd.hE<gt>> contains constants
 beginning with C<LIBNBD_STATE_> that may help decipher the values.
 On entry to the callback, the C<error> parameter contains the errno
-value of any previously detected error.
+value of any previously detected error, but even if an earlier error
+was detected, the current C<metacontext> and C<entries> are valid.
 
 It is possible for the extent function to be called
 more times than you expect (if the server is buggy),
@@ -2454,7 +2459,10 @@ Or supply the optional C<completion_callback> which will be invoked
 as described in L<libnbd(3)/Completion callbacks>.
 
 Note that you must ensure C<buf> is valid until the command has
-completed.  Other parameters behave as documented in L<nbd_pread(3)>."
+completed.  Furthermore, the contents of C<buf> are undefined if the
+C<error> parameter to C<completion_callback> is set, or if
+L<nbd_aio_command_completed(3)> reports failure.  Other parameters behave
+as documented in L<nbd_pread(3)>."
 ^ strict_call_description;
     example = Some "examples/aio-connect-read.c";
     see_also = [SectionLink "Issuing asynchronous commands";
@@ -2478,7 +2486,11 @@ To check if the command completed, call L<nbd_aio_command_completed(3)>.
 Or supply the optional C<completion_callback> which will be invoked
 as described in L<libnbd(3)/Completion callbacks>.
 
-Other parameters behave as documented in L<nbd_pread_structured(3)>."
+Note that you must ensure C<buf> is valid until the command has
+completed.  Furthermore, the contents of C<buf> are undefined if the
+C<error> parameter to C<completion_callback> is set, or if
+L<nbd_aio_command_completed(3)> reports failure.  Other parameters behave
+as documented in L<nbd_pread_structured(3)>."
 ^ strict_call_description;
     see_also = [SectionLink "Issuing asynchronous commands";
                 Link "aio_pread"; Link "pread_structured";

commit b88f1507909367c1b77a36551e22931d81795584

api: Tighten rules on completion.callback
The documentation has claimed since commit 6f4dcdab that any
completion callback will be called exactly once; but this is not
consistent with the code: if nbd_aio_* itself returns an error, then
nothing is queued and the user does not need to wait for a completion
callback to know how the command failed.  We could tweak the generator
to call completion.callback no matter what, but since the
completion.free callback already serves that role, it's easier to fix
the documentation to match reality.  After all, one only needs
completion status if an aio command returned success (if it returned
failure, we know that there is nothing that is going to complete
later).

However, there was one place where we indeed fail to call
completion.callback, even though the corresponding aio call returned
success, which can strand a user that was depending on the callback to
know that the pending aio command failed after all.  That's when a
call to nbd_close() interrupts a connection while commands are in
flight.  This problem appears to have been around even before commit
52b9b492 (v0.9.8) when we finally settled on having .free callbacks in
the first place.

Beef up the closure-lifetimes unit test to more robustly check the
various conditions guaranteed by the updated documentation, and to
expose the previous skip of a completion callback during nbd_close.

In summary, the behavior we want (where sequence is important) is:

- aio command fails:
mid-command .callback: 0 calls
completion .callback: 0 calls
mid-command .free: 1 call
completion .free: 1 call

- aio command succeeds:
mid-command .callback: 0, 1, or multiple calls
completion .callback: 1 call
mid-command .free: 1 call
completion .free: 1 call

Reported-by: Tage Johansson <tage.j.lists@posteo.net>
Fixes: 6f4dcdab ("docs: Clarify how callbacks should handle errors", v1.11.8)
Message-ID: <20230713193138.1306093-2-eblake@redhat.com>

docs/libnbd.pod
@@ -171,8 +171,12 @@ There are several things to note here:
 
 =item *
 
-This only starts the command.  The command is still in flight when the
-call returns.
+This only starts the command.  The command is (usually) still in
+flight when the call returns success, where you must rely on
+subsequent API calls for learning the final command outcome and
+trigger any remaining callbacks.  However, you must also be able to
+handle the case where system load allows the state machine to advance
+far enough to invoke callbacks before the asynchronous API returns.
 
 =item *
 
@@ -194,7 +198,10 @@ calls.  The cookie is unique (per libnbd handle) and E<ge> 1.
 
 You may register a function which is called when the command
 completes, see L</Completion callbacks> below.  In this case we have
-specified a null completion callback.
+specified a null completion callback.  If a completion callback is
+specified, it will only be called if the asynchronous command was
+sucessfully submitted (if the asynchronous API itself returns an
+error, there is nothing further to be completed).
 
 =back
 
@@ -897,16 +904,25 @@ asynchronous commands are retired.
 
 =head2 Callbacks and locking
 
-The callbacks are invoked at a point where the libnbd lock is held; as
-such, it is unsafe for the callback to call any C<nbd_*> APIs on the
-same nbd object, as it would cause deadlock.
+The callbacks are invoked at a point where the libnbd lock is held,
+typically during a call to C<nbd_aio_notify_read>,
+C<nbd_aio_notify_write>, C<nbd_aio_poll>, or other call that can
+advance libnbd's state machine.  Depending on system load, it is even
+possible for a callback to reached before completion of the
+C<nbd_aio_*> call that specified the callback.  As such, it is unsafe
+for the callback to call any C<nbd_*> APIs on the same nbd object, as
+it would cause deadlock.
 
 =head2 Completion callbacks
 
 All of the asychronous commands have an optional completion callback
-function that is used right before the command is marked complete,
-after any mid-command callbacks have finished, and before any free
-functions.
+function that is used if the call to the asynchronous API reports
+success.  The completion callback is invoked when the submitted
+command is eventually marked complete, after any mid-command callbacks
+have finished, and before any free functions.  The completion callback
+is not reached if the asynchronous API itself fails, while free
+callbacks are reached regardless of the result of the initial
+asynchronous API.
 
 When the completion callback returns C<1>, the command is
 automatically retired (there is no need to call
@@ -946,13 +962,16 @@ mid-command callback may be reached more times than expected if the
 server is non-compliant.
 
 On the other hand, if a completion callback is supplied (only possible
-with asynchronous commands), it will always be reached exactly once,
-and the completion callback must not ignore the value pointed to by
-C<error>.  In particular, the content of a buffer passed to
-L<nbd_aio_pread(3)> or L<nbd_aio_pread_structured(3)> is undefined
-if C<*error> is non-zero on entry to the completion callback.  It is
-recommended that if you choose to use automatic command retirement
-(where the completion callback returns C<1> to avoid needing to call
+with asynchronous commands), it will not be called if the initial API
+call fails (such as attempting an asynchronous command in the wrong
+state - there is nothing to be completed since the command was not
+queued), but will otherwise be called exactly once, and the completion
+callback must not ignore the value pointed to by C<error>.  In
+particular, the content of a buffer passed to L<nbd_aio_pread(3)> or
+L<nbd_aio_pread_structured(3)> is undefined if C<*error> is non-zero
+on entry to the completion callback.  It is recommended that if you
+choose to use automatic command retirement (where the completion
+callback returns C<1> to avoid needing to call
 L<nbd_aio_command_completed(3)> later), your completion function
 should return C<1> on all control paths, even when handling errors
 (note that with automatic retirement, assigning into C<error> is

lib/handle.c
@@ -37,7 +37,10 @@ free_cmd_list (struct command *list)
   struct command *cmd, *cmd_next;
 
   for (cmd = list; cmd != NULL; cmd = cmd_next) {
+    int error = cmd->error ? : ENOTCONN;
+
     cmd_next = cmd->next;
+    CALL_CALLBACK (cmd->cb.completion, &error);
     nbd_internal_retire_and_free_command (cmd);
   }
 }

tests/closure-lifetimes.c
@@ -66,6 +66,7 @@ read_cb (void *opaque,
          uint64_t offset, unsigned status, int *error)
 {
   assert (!read_cb_freed);
+  assert (!completion_cb_called);
   read_cb_called++;
   return 0;
 }
@@ -73,6 +74,7 @@ read_cb (void *opaque,
 static void
 read_cb_free (void *opaque)
 {
+  assert (!completion_cb_freed);
   read_cb_freed++;
 }
 
@@ -81,6 +83,7 @@ block_status_cb (void *opaque, const char *meta, uint64_t offset,
                  uint32_t *entries, size_t nr_entries, int *error)
 {
   assert (!block_status_cb_freed);
+  assert (!completion_cb_called);
   block_status_cb_called++;
   return 0;
 }
@@ -88,6 +91,7 @@ block_status_cb (void *opaque, const char *meta, uint64_t offset,
 static void
 block_status_cb_free (void *opaque)
 {
+  assert (!completion_cb_freed);
   block_status_cb_freed++;
 }
 
@@ -150,6 +154,10 @@ main (int argc, char *argv[])
   cookie = nbd_aio_pread_structured (nbd, buf, sizeof buf, 0, chunk_callback,
                                      completion_callback, 0);
   if (cookie == -1) NBD_ERROR;
+  /* read_cb_called is indeterminate at this point, as state machine
+   * progress may vary based on task schduling and network speed factors.
+   */
+  assert (completion_cb_called == 0);
   assert (read_cb_freed == 0);
   assert (completion_cb_freed == 0);
   while (!nbd_aio_command_completed (nbd, cookie)) {
@@ -179,6 +187,8 @@ main (int argc, char *argv[])
   nbd_kill_subprocess (nbd, 0);
   nbd_close (nbd);
 
+  /* read_cb_called is indeterminate based on timing of kill. */
+  assert (completion_cb_called == 1);
   assert (read_cb_freed == 1);
   assert (completion_cb_freed == 1);
 
@@ -198,6 +208,8 @@ main (int argc, char *argv[])
     fprintf (stderr, "%s: Expecting block_status failure\n", argv[0]);
     exit (EXIT_FAILURE);
   }
+  assert (block_status_cb_called == 0);
+  assert (completion_cb_called == 0);
   assert (block_status_cb_freed == 1);
   assert (completion_cb_freed == 1);
 
@@ -212,6 +224,8 @@ main (int argc, char *argv[])
     fprintf (stderr, "%s: Expecting block_status failure\n", argv[0]);
     exit (EXIT_FAILURE);
   }
+  assert (block_status_cb_called == 0);
+  assert (completion_cb_called == 0);
   assert (block_status_cb_freed == 1);
   assert (completion_cb_freed == 1);
 