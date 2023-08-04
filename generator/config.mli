ebblake/libnbd

commit b2511d640bc12b0116e6c013a17aefb4e772a058

rust: create basic Rust bindings
This commit creates basic Rust bindings in the rust directory.
The bindings are generated by generator/Rust.ml and
generator/RustSys.ml.

Reviewed-by: Richard W.M. Jones <rjones@redhat.com>

RWMJ:

 - Alphabetical order for some things added to Makefile.am & configure.ac

 - Make sure the generator uses @RUSTFMT@ instead of invoking
   rustfmt directly, so that ./configure RUSTFMT=... works

generator/config.ml.in
@@ -1,0 +1,21 @@
+(* hey emacs, this is OCaml code: -*- tuareg -*- *)
+(* nbd client library in userspace: utilities
+ * Copyright Red Hat
+ * @configure_input@
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *)
+
+let rustfmt  = "@RUSTFMT@"

generator/config.mli
@@ -1,0 +1,20 @@
+(* hey emacs, this is OCaml code: -*- tuareg -*- *)
+(* nbd client library in userspace: utilities
+ * Copyright Red Hat
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *)
+
+val rustfmt : string

generator/utils.ml
@@ -432,7 +432,8 @@ let output_to ?(formatter = None) filename k =
   chan := NoOutput;
   (match formatter with
   | Some Rustfmt ->
-    (match system (sprintf "rustfmt %s" filename_new) with
+    let cmd = sprintf "%s %s" Config.rustfmt filename_new in
+    (match system cmd with
       | WEXITED 0 -> ()
       | WEXITED i -> failwith (sprintf "Rustfmt failed with exit code %d" i)
       | _ -> failwith "Rustfmt was killed or stopped by a signal.");

.gitignore
@@ -88,6 +88,7 @@ Makefile.in
 /fuzzing/libnbd-libfuzzer-test
 /fuzzing/sync_dir/
 /fuzzing/testcase_dir/[0-f]*
+/generator/config.ml
 /generator/generator
 /generator/generator-cache.v1
 /generator/stamp-generator
@@ -174,6 +175,16 @@ Makefile.in
 /python/nbd.py
 /python/run-python-tests
 /run
+/rust/Cargo.lock
+/rust/libnbd-sys/Cargo.lock
+/rust/libnbd-sys/libnbd_version
+/rust/libnbd-sys/src/generated.rs
+/rust/src/async_bindings.rs
+/rust/src/bindings.rs
+/rust/target
+/rust/cargo_test/Cargo.lock
+/rust/cargo_test/target
+/rust/run-tests.sh
 /sh/nbdsh
 /sh/nbdsh.1
 /stamp-h1

Makefile.am
@@ -27,6 +27,7 @@ EXTRA_DIST = \
 	.ocamlformat \
 	html/pod.css \
 	README.md \
+	rustfmt.toml \
 	scripts/git.orderfile \
 	SECURITY \
 	$(NULL)
@@ -56,6 +57,7 @@ SUBDIRS = \
 	ocaml/tests \
 	golang \
 	golang/examples \
+	rust \
 	interop \
 	fuzzing \
 	bash-completion \

configure.ac
@@ -613,6 +613,32 @@ AS_IF([test "x$enable_golang" != "xno"],[
 ],[GOLANG=no])
 AM_CONDITIONAL([HAVE_GOLANG],[test "x$GOLANG" != "xno"])
 
+dnl Rust.
+AC_ARG_ENABLE([rust],
+    AS_HELP_STRING([--disable-rust], [disable Rust language bindings]),
+        [],
+        [enable_rust=yes])
+AS_IF([test "x$enable_rust" != "xno"],[
+    AC_CHECK_PROG([CARGO],[cargo],[cargo],[no])
+    AC_CHECK_PROG([RUSTFMT],[rustfmt],[rustfmt],[no])
+    AS_IF([test "x$CARGO" != "xno"],[
+        AC_MSG_CHECKING([if $CARGO is usable])
+        AS_IF([ (
+              cd $srcdir/rust/cargo_test &&
+               $CARGO test 2>&AS_MESSAGE_LOG_FD 1>&2 &&
+               $CARGO doc 2>&AS_MESSAGE_LOG_FD 1>&2 &&
+               $CARGO fmt 2>&AS_MESSAGE_LOG_FD 1>&2
+               ) ],[
+            AC_MSG_RESULT([yes])
+        ],[
+            AC_MSG_RESULT([no])
+            AC_MSG_WARN([Rust ($CARGO) is installed but not usable])
+            CARGO=no
+        ])
+    ])
+],[CARGO=no])
+AM_CONDITIONAL([HAVE_RUST],[test "x$CARGO" != "xno" -a "x$RUSTFMT" != "xno"])
+
 AC_MSG_CHECKING([for how to mark DSO non-deletable at runtime])
 NODELETE=
 `$LD --help 2>&1 | grep -- "-z nodelete" >/dev/null` && \
@@ -641,6 +667,8 @@ AC_CONFIG_FILES([python/run-python-tests],
                 [chmod +x,-w python/run-python-tests])
 AC_CONFIG_FILES([run],
                 [chmod +x,-w run])
+AC_CONFIG_FILES([rust/run-tests.sh],
+                [chmod +x,-w rust/run-tests.sh])
 AC_CONFIG_FILES([sh/nbdsh],
                 [chmod +x,-w sh/nbdsh])
 
@@ -655,6 +683,7 @@ AC_CONFIG_FILES([Makefile
                  fuse/Makefile
                  fuzzing/Makefile
                  generator/Makefile
+                 generator/config.ml
                  golang/Makefile
                  golang/examples/Makefile
                  include/Makefile
@@ -669,6 +698,7 @@ AC_CONFIG_FILES([Makefile
                  ocaml/examples/Makefile
                  ocaml/tests/Makefile
                  python/Makefile
+                 rust/Makefile
                  sh/Makefile
                  tests/Makefile
                  tests/functions.sh
@@ -719,6 +749,7 @@ echo
 feature "Go"                    test "x$HAVE_GOLANG_TRUE" = "x"
 feature "OCaml"                 test "x$HAVE_OCAML_TRUE" = "x"
 feature "Python"                test "x$HAVE_PYTHON_TRUE" = "x"
+feature "Rust"                  test "x$HAVE_RUST_TRUE" = "x"
 
 echo
 echo "Server interoperability testing:"

generator/Makefile.am
@@ -44,6 +44,8 @@ states_code = \
 
 # Source code for the generator.
 sources = \
+	config.mli \
+	config.ml \
 	utils.mli \
 	utils.ml \
 	state_machine.mli \
@@ -60,6 +62,10 @@ sources = \
 	OCaml.ml \
 	GoLang.mli \
 	GoLang.ml \
+	RustSys.mli \
+	RustSys.ml \
+	Rust.mli \
+	Rust.ml \
 	generator.ml \
 	$(NULL)
 

commit bf3deb07dfcb068bbd45b60acc4948371039401e

generator: Handle ./configure --disable-rust
If this option is used, then "@RUSTFMT@" is expanded to "" (not "no").
Let's change the Config.rustfmt value to be an option type, while also
fixing this problem.

Fixes: commit b2511d640bc12b0116e6c013a17aefb4e772a058

generator/config.ml.in
@@ -18,4 +18,4 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  *)
 
-let rustfmt  = "@RUSTFMT@"
+let rustfmt = match "@RUSTFMT@" with "" | "no" -> None | s -> Some s

generator/config.mli
@@ -17,4 +17,4 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  *)
 
-val rustfmt : string
+val rustfmt : string option

generator/utils.ml
@@ -432,13 +432,16 @@ let output_to ?(formatter = None) filename k =
   chan := NoOutput;
   (match formatter with
   | Some Rustfmt ->
-     if Config.rustfmt <> "no" then (
-       let cmd = sprintf "%s %s" Config.rustfmt filename_new in
-       match system cmd with
-       | WEXITED 0 -> ()
-       | WEXITED i -> failwithf "rustfmt failed with exit code %d" i
-       | WSIGNALED i | WSTOPPED i ->
-          failwithf "rustfmt was killed or stopped by signal %d" i
+     (match Config.rustfmt with
+      | Some rustfmt ->
+         (let cmd = sprintf "%s %s" rustfmt filename_new in
+          match system cmd with
+          | WEXITED 0 -> ()
+          | WEXITED i -> failwithf "rustfmt failed with exit code %d" i
+          | WSIGNALED i | WSTOPPED i ->
+             failwithf "rustfmt was killed or stopped by signal %d" i
+         )
+      | None -> ()
      );
   | None -> ());
   (* Is the new file different from the current file? *)