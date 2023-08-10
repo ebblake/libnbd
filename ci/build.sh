ebblake/libnbd

commit 791ed5cd947f70ed0683e45aebf1d7394ff84dab

ci: Add i686 builds
- ci/refresh: add generation for i686 connections
- ci/build.sh: indentation fix and i686-related changes
- ci/{containers,cirrus}: regenerate files
- .gitlab-ci.yaml: add i686 builds

Signed-off-by: Martin Kletzander <mkletzan@redhat.com>

ci/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -e
+set -xe
 
 skip_tests() {
     # Add a way to run all the tests, even the skipped ones, with an environment
@@ -72,24 +72,34 @@ main() {
 
     CONFIG_ARGS="\
 --enable-gcc-warnings \
---enable-fuse \
---enable-ocaml \
---enable-python \
---enable-golang \
 --with-gnutls \
 --with-libxml2 \
 "
 
-    if test "$GOLANG" != "skip"
+    if "$CROSS"
     then
-       CONFIG_ARGS="$CONFIG_ARGS --enable-golang"
+        CONFIG_ARGS="$CONFIG_ARGS
+            --disable-fuse
+            --disable-ocaml
+            --disable-python"
+    else
+        CONFIG_ARGS="$CONFIG_ARGS
+            --enable-fuse
+            --enable-ocaml
+            --enable-python"
+        if test "$GOLANG" = "skip"
+        then
+            CONFIG_ARGS="$CONFIG_ARGS --disable-golang"
+        else
+            CONFIG_ARGS="$CONFIG_ARGS --enable-golang"
+        fi
     fi
 
     ./configure $CONFIGURE_OPTS $CONFIG_ARGS
 
     $MAKE
 
-    if test -n "$CROSS"
+    if test -n "$CROSS" -a "$CROSS" != "i686"
     then
         echo "Possibly run tests with an emulator in the future"
         return 0

.gitlab-ci.yml
@@ -95,6 +95,17 @@ stages:
   rules:
     - if: "$CIRRUS_GITHUB_REPO && $CIRRUS_API_TOKEN"
 
+# Cross-compilation container build jobs
+i686-debian-10-container:
+  extends: .container_job
+  variables:
+    NAME: debian-10-cross-i686
+
+i686-debian-11-container:
+  extends: .container_job
+  variables:
+    NAME: debian-11-cross-i686
+
 # Native container build jobs
 x64-alpine-314-container:
   extends: .container_job
@@ -116,10 +127,10 @@ x64-centos-stream-8-container:
   variables:
     NAME: centos-stream-8
 
-x64-debian-10-container:
+x64-debian-11-container:
   extends: .container_job
   variables:
-    NAME: debian-10
+    NAME: debian-11
 
 x64-debian-sid-container:
   extends: .container_job
@@ -162,14 +173,30 @@ x64-ubuntu-2004-container:
   variables:
     NAME: ubuntu-2004
 
-# Native architecture build + test jobs
-
-x64-debian-10:
+# Cross-compilation build jobs
+i686-debian-10:
   extends: .native_build_job
   needs:
-    - x64-debian-10-container
+    - i686-debian-10-container
   variables:
     NAME: debian-10
+    CROSS: i686
+
+i686-debian-11:
+  extends: .native_build_job
+  needs:
+    - i686-debian-11-container
+  variables:
+    NAME: debian-11
+    CROSS: i686
+
+# Native architecture build + test jobs
+x64-debian-11:
+  extends: .native_build_job
+  needs:
+    - x64-debian-11-container
+  variables:
+    NAME: debian-11
 
 x64-debian-sid:
   extends: .native_build_job

ci/cirrus/freebsd-12.vars
@@ -2,7 +2,7 @@
 #
 #  $ lcitool variables freebsd-12 libnbd
 #
-# https://gitlab.com/libvirt/libvirt-ci/-/commit/0bb9bfada8e143e05bb436a06747d227d19f0df4
+# https://gitlab.com/libvirt/libvirt-ci
 
 PACKAGING_COMMAND='pkg'
 CCACHE='/usr/local/bin/ccache'

ci/cirrus/freebsd-13.vars
@@ -2,7 +2,7 @@
 #
 #  $ lcitool variables freebsd-13 libnbd
 #
-# https://gitlab.com/libvirt/libvirt-ci/-/commit/0bb9bfada8e143e05bb436a06747d227d19f0df4
+# https://gitlab.com/libvirt/libvirt-ci
 
 PACKAGING_COMMAND='pkg'
 CCACHE='/usr/local/bin/ccache'

ci/cirrus/freebsd-current.vars
@@ -2,7 +2,7 @@
 #
 #  $ lcitool variables freebsd-current libnbd
 #
-# https://gitlab.com/libvirt/libvirt-ci/-/commit/0bb9bfada8e143e05bb436a06747d227d19f0df4
+# https://gitlab.com/libvirt/libvirt-ci
 
 PACKAGING_COMMAND='pkg'
 CCACHE='/usr/local/bin/ccache'

ci/cirrus/macos-11.vars
@@ -2,7 +2,7 @@
 #
 #  $ lcitool variables macos-11 libnbd
 #
-# https://gitlab.com/libvirt/libvirt-ci/-/commit/0bb9bfada8e143e05bb436a06747d227d19f0df4
+# https://gitlab.com/libvirt/libvirt-ci
 
 PACKAGING_COMMAND='brew'
 CCACHE='/usr/local/bin/ccache'

ci/containers/alpine-314.Dockerfile
@@ -2,7 +2,7 @@
 #
 #  $ lcitool dockerfile alpine-314 libnbd
 #
-# https://gitlab.com/libvirt/libvirt-ci/-/commit/0bb9bfada8e143e05bb436a06747d227d19f0df4
+# https://gitlab.com/libvirt/libvirt-ci
 
 FROM docker.io/library/alpine:3.14
 

ci/containers/alpine-edge.Dockerfile
@@ -2,7 +2,7 @@
 #
 #  $ lcitool dockerfile alpine-edge libnbd
 #
-# https://gitlab.com/libvirt/libvirt-ci/-/commit/0bb9bfada8e143e05bb436a06747d227d19f0df4
+# https://gitlab.com/libvirt/libvirt-ci
 
 FROM docker.io/library/alpine:edge
 

commit 7a70a87ea562a23f18774182f5ed36698826b451

maint: avoid non-portable 'test ... -a ...'
POSIX says test -a/-o are not required to work.  Use two test commands
instead.

Fixes: 791ed5cd94 ("ci: Add i686 builds", v1.9.5)
Signed-off-by: Eric Blake <eblake@redhat.com>

ci/build.sh
@@ -112,7 +112,7 @@ main() {
 
     $MAKE
 
-    if test -n "$CROSS" -a "$CROSS" != "i686"
+    if test -n "$CROSS" && test "$CROSS" != "i686"
     then
         echo "Possibly run tests with an emulator in the future"
         return 0